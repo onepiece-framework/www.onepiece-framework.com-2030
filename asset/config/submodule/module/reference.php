ee365e202d99c8b9b0bf117079047cc9efd773a2
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-04 16:59:44 +0900

    New: asset > config > submodule > module > *

---
+++ asset/config/submodule/module/reference.php
@@ -0,0 +1,23 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/module/reference.php
+ *
+ * @created    2026-01-03
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'url'    => 'https://github.com/onepiece-framework/op-module-reference.git',
+];
