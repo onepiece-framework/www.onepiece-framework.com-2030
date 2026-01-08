6aa524ae5ab1983ef94c15cc4c7184e4cb41a3f8
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-08 15:59:31 +0900

    New: asset > config > git-rebase-rules.php | prefix, deny

---
+++ asset/config/git-rebase-rules.php
@@ -0,0 +1,41 @@
+<?php
+/**	op-skeleton-2030:/asset/config/git-rebase-rules.php
+ *
+ * @created    2025-01-08
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	//	Allowed commit message prefix.
+	'prefix' => [
+		'New: ',
+		'Add: ',
+		'Chg: ',
+		'Fix: ',
+		'Del: ',
+		'Doc: ',
+		'2024: ',
+		'2025: ',
+		'2026: ',
+		'2027: ',
+		'2028: ',
+		'2029: ',
+		'2030: ',
+	],
+	//	Deny commit message word.
+	'deny'   => [
+	],
+];
