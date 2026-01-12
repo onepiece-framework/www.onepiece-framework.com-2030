7352d9747ddc3f6580ad8451bbea836187c3a7ce
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-13 00:19:48 +0900

    onepiece: asset > config > cd.php | New > branch > onepiece

---
+++ asset/config/cd.php
@@ -0,0 +1,25 @@
+<?php
+/**	www.onepiece-framework.com-2030:/asset/config/cd.php
+ *
+ * @created    2026-01-13
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'branch' => [
+		'onepiece',
+	],
+];
