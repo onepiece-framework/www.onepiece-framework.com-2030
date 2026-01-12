a98f4c099db848f9f460a214ab7ace314c49ab3b
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-12 18:59:14 +0900

    New: asset > config > submodule > public_html > api.php, www.php

---
+++ asset/config/submodule/public_html/api.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/public_html/api.php
+ *
+ * @created    2026-01-12
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'path'   => 'public_html_api',
+	'url'    => 'https://github.com/onepiece-framework/op-public-api.git',
+];
