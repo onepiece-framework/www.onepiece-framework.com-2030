8e6aa4eb4fd0cae8352b33b9411deb2552191529
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-02-08 13:06:21 +0900

    New: asset > config > submodule > asset > core.php

---
+++ asset/config/submodule/asset/core.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/asset/core.php
+ *
+ * @created    2026-02-08
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'path'   => 'core',
+	'url'    => 'https://github.com/onepiece-framework/op-core-8.git',
+];
