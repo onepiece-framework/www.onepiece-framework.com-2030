565231c6552f2e94f08b604c0147ac11c8ff9523
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-02-14 00:09:07 +0900

    New: asset > config > submodule > unit > bitcoin-rpc.php

---
+++ asset/config/submodule/unit/bitcoin-rpc.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/unit/bitcoin-rpc.php
+ *
+ * @created    2026-01-17
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'path'   => 'bitcoin-rpc',
+	'url'    => 'https://github.com/onepiece-framework/op-unit-bitcoin-rpc.git',
+];
