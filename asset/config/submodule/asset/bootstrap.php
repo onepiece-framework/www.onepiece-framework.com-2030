19e88a1405e4f0e67bd499e48a2678d012373aff
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-02-25 21:38:05 +0900

    New: asset > config > submodule > asset > bootstrap.php, hooks.php, template.php

---
+++ asset/config/submodule/asset/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/asset/bootstrap.php
+ *
+ * @created    2026-02-09
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  0,
+	'path'   => 'bootstrap',
+	'url'    => 'https://github.com/onepiece-framework/op-asset-bootstrap2.git',
+];
