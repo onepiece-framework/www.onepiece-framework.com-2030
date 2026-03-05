b3f4582d980c0a9186677efef05e9d5a8bdc2363
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-06 03:33:46 +0900

    Fix: asset > init > function > GitSubmoduleGithub.php | Git submodule init and update.

--- asset/init/function/GitSubmoduleGithub.php
+++ asset/init/function/GitSubmoduleGithub.php
@@ -28,22 +28,22 @@ require_once(__DIR__.'/Request.php');
  */
 function GitSubmoduleGithub()
 {
-	//	...
-	if(!$github = Request('github') ){
-		return;
-	}
-
-	//	...
+	//	If submodule not exists then return.
 	if(!file_exists('.gitmodules')){
 		return;
 	}
 
+	//	If specify github account then replace github account in .gitmodules.
+	if( $github = Request('github') ){
 	//	...
 	`cp .gitmodules .gitmodules_origin`;
 
 	//	...
 	`sed -i -e "s/onepiece-framework/{$github}/g" .gitmodules`;
+	}
 
-	//	...
+	//	Git clone submodules.
 	`git submodule sync`;
+	`git submodule init`;
+	`git submodule update`;
 }
