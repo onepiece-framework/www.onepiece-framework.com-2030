9ca4fa5e13a9ea0030bd177c4750aa573ecaec1a
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-09 21:06:11 +0900

    New: asset > init > function > Update.php

---
+++ asset/init/function/Update.php
@@ -0,0 +1,80 @@
+<?php
+/**	op-skeleton-2030:/asset/init/function/Update.php
+ *
+ * @created    2026-04-07
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\SKELETON\INIT;
+
+/**	Update OP managed Git repositories.
+ *
+ * @created    2026-01-04 op-skeleton-2030:/asset/init/update.php
+ * @created    2026-04-07 op-skeleton-2030:/asset/init/function/Update.php
+ * @param      string     $type
+ * @param      string     $name
+ * @param      array      $config
+ * @return     bool
+ */
+function Update( string $type, string $name, array $config, bool $init ) : bool
+{
+	//	Init
+	$dir    = Dir($type);
+	$path   = $config['path']   ?? $name;
+	$remote = $config['remote'] ?? 'origin';
+	$branch = $config['branch'] ?? _OP_APP_BRANCH_;
+
+	//	Escape
+	$remote = escapeshellarg($remote);
+	$branch = escapeshellarg($branch);
+
+	//	Check direcory exists.
+	if(!file_exists("{$dir}/{$path}") ){
+		return true;
+	}
+
+	//	Change directory.
+	chdir("{$dir}/{$path}");
+
+	//	Check flag
+	if( $init ){
+
+	}else{
+		//	Args
+		$target = Request('remote', '--all');
+
+		//	Fetch
+		`git fetch {$target}`;
+
+		//	Pull
+		if( Request('pull', '') ){
+			`git pull {$remote} {$branch}`;
+		}
+
+		//	Submodule
+		if( file_exists('.gitmodules') ){
+			$save_dir = getcwd();
+			foreach( GitSubmoduleConfig('.gitmodules', getcwd()) as $config ){
+				chdir($config['path']);
+				if( Execute("git fetch {$target}") ){
+					Execute("git pull {$remote} {$branch}");
+				}
+				chdir($save_dir);
+			}
+			chdir($save_dir);
+		}
+	}
+
+	//	...
+	return true;
+}
