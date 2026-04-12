2684d6e93611de5b10bf6d68fa65af3b9dc589eb
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-12 14:42:37 +0900

    New: asset > config > update.php

---
+++ asset/config/update.php
@@ -0,0 +1,31 @@
+<?php
+/**	op-skeleton-2030:/asset/config/update.php
+ *
+ * @created    2026-04-12
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	Return
+ *
+ */
+return [
+	/*
+	'github' => 'TomoakiNagahara',
+	'local'  =>  1,
+	'ssh'    =>  1,
+	'host'   => 'arch',
+	'path'   => '~/repo/',
+	*/
+];
