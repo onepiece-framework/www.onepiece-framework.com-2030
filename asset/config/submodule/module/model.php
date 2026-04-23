251f944638090ad091767a12b5e4709cf43d3e09
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-23 14:10:30 +0900

    New: asset > config > submodule > module > model.php

---
+++ asset/config/submodule/module/model.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/module/model.php
+ *
+ * @created    2026-04-23
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'url'    => 'https://github.com/onepiece-framework/op-module-model.git',
+];
