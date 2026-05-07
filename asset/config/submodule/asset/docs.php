7b08fc7589d91b56f907c93bccce5cfae01cafd2
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-07 12:20:59 +0900

    New: asset > config > submodule > asset > docs.php

---
+++ asset/config/submodule/asset/docs.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/asset/docs.php
+ *
+ * @created    2026-04-29
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'path'   => 'docs',
+	'url'    => 'https://github.com/onepiece-framework/op-asset-docs.git',
+];
