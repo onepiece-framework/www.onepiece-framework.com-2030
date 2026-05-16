377763a2fbea192f7900ce9ed66dc647bf709e92
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-17 03:02:14 +0900

    New: asset > config > submodule > module > counter.php

---
+++ asset/config/submodule/module/counter.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/module/counter.php
+ *
+ * @created    2026-05-16
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'url'    => 'https://github.com/TomoakiNagahara/op-module-counter.git',
+];
